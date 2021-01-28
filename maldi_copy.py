cbc6e611dea56e374416e9f94891e62a6ebfea1c
sipocz <sipoczlaszlo@gmail.com>
2021-01-28 22:58:35 +0100

    20210128

---
+++ maldi_copy.py
@@ -0,0 +1,14 @@
+
+
+# path definition
+
+DOR_prefix="DOR"
+DEB_prefix="DEB"
+BUD_prefix="BUD"
+
+DOR_Source_Path="\\\\hungary\\dfsroot\\Maldi_eredmenyek\\Dorog kezi"
+DEB_Source_Path="\\\\hungary\\dfsroot\\Maldi_eredmenyek\\Dorog kezi"
+BUD_Source_Path="\\\\hungary\\dfsroot\\Maldi_eredmenyek\\Dorog kezi"
+
+
+
